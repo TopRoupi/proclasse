66ca6e1aad505217c39c9a495e6b39c9b1826d99
breno <breno.nunes@segarante.com.br>
2024-11-16 20:37:14 -0300

    fix login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,19 @@ class ApplicationController < ActionController::Base
   # allow_browser versions: :modern
 
   before_action :set_current_request_details
+  before_action :set_current_user
   before_action :authenticate
 
   private
     def authenticate
+      unless Current.session
+        redirect_to sign_in_path
+      end
+    end
+
+    def set_current_user
       if session_record = Session.find_by_id(cookies.signed[:session_token])
         Current.session = session_record
-      else
-        redirect_to sign_in_path
       end
     end
 
